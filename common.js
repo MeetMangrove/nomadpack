b0ba4f3d9f1e351ace86cf6660369a03fa671ffa
Thomas Chrétien <thomas@chretien.co>
2016-11-21 19:42:04 +0700

    popover

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ var curators = document.getElementById('curators');
 function toggleCurators() {
   curators.style.display = curators.style.display == 'none' ? 'block' : 'none';
 }
-var toggle = document.getElementById('js-curators-toggle').parentNode;
+var toggle = document.getElementById('js-curators-toggle');
 toggle.onmouseenter = toggle.onmouseleave = toggleCurators;
 
 var tweetBtn = document.getElementById('btn-tweet');
